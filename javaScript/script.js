f7ac8c785e1b7ec112aef0f4369c611eab4de545
Isaaczl <isaac.ramlop01@gmail.com>
2022-01-19 13:39:42 -0600

    Agregué información y estilos al DOM

---
+++ javaScript/script.js
@@ -0,0 +1,23 @@
+const father = document.querySelector(`.main`);
+
+const paragraphInstructions = document.createElement(`P`)
+
+paragraphInstructions.innerHTML = `
+<b>git init :</b> Inicializa un proyecto en git mientras se está en la ruta del proyecto<br>
+<b>git status :</b><br>
+<b>git add . :</b><br>
+<b>git commit -m "example" :</b><br>
+<b>git config --global user.email "mail@mail.com":</b><br>
+<b>git config --global user.name "user":</b><br>
+<b>git log :</b><br>
+<b>git diff route.ext :</b><br>
+<b>git branch :</b><br>
+<b>git branch newBranch :</b><br>
+<b>git checkout selectBranch :</b><br>
+<b>git remote add origin https://link.git :</b><br>
+<b>git push -u origin master :</b><br>
+<b>git clone https://repostory.git :</b><br>
+`;
+
+father.appendChild(paragraphInstructions);
+
